3393ccadcf459704148bdd304d73efe75adef450
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-11-25 13:19:39 -0500

    feat: test the model hook

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,25 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
   emailAddress: '',
-  isDisabled: computed('emailAddress', function() {
-    return this.get('emailAddress') === '';
-  })
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+  responseMessage: '',
+  actions: {
+    saveInvitation() {
+      alert(
+        `Saving of the following email address is in progress: ${this.get(
+          'emailAddress'
+        )}`
+      );
+      this.set(
+        'responseMessage',
+        `Thank you! We've just saved your email address: ${this.get(
+          'emailAddress'
+        )}`
+      );
+      this.set('emailAddress', '');
+    }
+  }
 });
--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        id: 'something',
+        title: 'Something'
+      },
+      {
+        id: 'something else',
+        title: 'testing something else'
+      }
+    ];
+  }
 });
